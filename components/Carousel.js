7f214f1f5b10024c8f5ddb46bc1bb9246d1ca070
devbeau <48367657+devbeau@users.noreply.github.com>
2020-08-07 15:56:55 -0400

    stretch filter

--- components/Carousel.js
+++ components/Carousel.js
@@ -22,3 +22,78 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function createHTMLElement(htmlElement, className = 0, textContent = 0){ // create elements with class and textcontent
+  let element = document.createElement(htmlElement);
+  className !== 0 ? element.classList.add(className) : element;
+  textContent !== 0 ? element.textContent = textContent : element.textContent = null;
+  return element;
+}
+
+function createImageElement(source, id){
+ let image =  createHTMLElement('img');
+ image.setAttribute('src', source);
+ image.id = id;
+//  image.style.display = 'inline-block';
+ image.style.border = '3px darkgrey solid';
+ image.style.borderRadius = '200px';
+ return image;
+}
+
+function carouselButton(button, imageArray, forward = true){
+  let state = 1; 
+  return (button.addEventListener('click', (event) => {
+    if (forward){
+      state === 4 ? state = 1 : state += 1;
+      } else {
+      state === 1 ? state = 4 : state -= 1;
+      }
+
+    imageArray.forEach(image => {
+      let splitString= image.id.split("");
+      let imgNum = Number.parseInt(splitString[splitString.length -1]);
+      console.log(imgNum);
+      console.log(splitString);
+      if ( imgNum === state){
+        image.style.display = 'inline-block';
+      } else{
+        image.style.display = 'none';
+      }
+      console.log(state);
+    })
+  })
+  )
+}
+
+function createCarousel(){
+  let carouselDiv = createHTMLElement('div', 'carousel');
+  let leftButton = createHTMLElement('div', 'left-button');
+  let firstImage = createImageElement('https://tk-assets.lambdaschool.com/ba687af4-3a1e-43d7-87b2-f30453264c9d_mountains.jpeg', 'image-1');
+  let secondImage = createImageElement('https://tk-assets.lambdaschool.com/8aa075b0-67cf-47ce-9a7f-8cc9d754675d_computer.jpeg', 'image-2');
+  let thirdImage = createImageElement('https://tk-assets.lambdaschool.com/5b7441c6-6e4b-4feb-a4ec-8dd2eb76238a_trees.jpeg', 'image-3');
+  let fourthImage = createImageElement('https://tk-assets.lambdaschool.com/0b770382-d0eb-4465-8bf2-692a79fcda71_turntable.jpeg', 'image-4');
+  let rightButton = createHTMLElement('div', 'right-button');
+
+  carouselDiv.appendChild(leftButton);
+  carouselDiv.appendChild(firstImage);
+  carouselDiv.appendChild(secondImage);
+  carouselDiv.appendChild(thirdImage);
+  carouselDiv.appendChild(fourthImage);
+  carouselDiv.appendChild(rightButton);
+
+  let imageArray = [firstImage, secondImage, thirdImage, fourthImage];
+
+  let btnForward = carouselButton(rightButton, imageArray, true);
+  let btnBackward = carouselButton(leftButton, imageArray, false);
+
+  btnForward;
+  btnBackward;
+
+  firstImage.style.display = 'inline-block';
+
+  firstImage.style
+  let container = document.querySelector('.carousel-container');
+  return container.appendChild(carouselDiv);
+}
+
+createCarousel();
